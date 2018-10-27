6ebe805cb63deaac6542505d2166f080f8cc2e8d
victorsteven <chikodi543@gmail.com>
2018-10-27 04:31:01 +0100

    product collection resource created

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Model\Product;
 use Illuminate\Http\Request;
 use App\Http\Resources\Product\ProductResource;
+use App\Http\Resources\Product\ProductCollection;
 
 class ProductController extends Controller
 {
@@ -15,7 +16,11 @@ class ProductController extends Controller
      */
     public function index()
     {
-        return Product::all();
+        // return Product::all();
+        // return ProductResource::collection(Product::all());
+
+        // return new ProductCollection(Product::all()); //this will transform only one product, to transform all products, we use collection:
+        return ProductCollection::collection(Product::all());
     }
 
     /**
